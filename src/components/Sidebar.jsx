8231bf0ebb521027e59187b19789ad32f5e4f97b
Brenda Renata de Souza <si.brendarenata@gmail.com>
2025-06-14 20:40:21 -0300

    feat: create component Avatar with props logic

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
 export function Sidebar() {
@@ -9,8 +10,7 @@ export function Sidebar() {
 
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/brendarsouza.png" />
-
+                <Avatar src="https://github.com/brendarsouza.png" />
                 <strong>Brenda Renata</strong>
                 <span>Web Developer</span>
             </div>
